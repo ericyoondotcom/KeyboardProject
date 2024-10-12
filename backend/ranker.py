d31801edc8ea1a7cf893f5075cba0a4ab66d8ad2
Joshua Chen <sidachen2003@gmail.com>
2024-10-12 09:27:46 -0400

    Use semantic embedding instead

--- backend/ranker.py
+++ backend/ranker.py
@@ -1,22 +1,24 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-import numpy as np
+from sentence_transformers import SentenceTransformer, util
 import pandas as pd
 
 data = pd.read_csv("emoji_transformed.csv", keep_default_na=False)
 
-input_text = "crying"
+input_text = "vegetable"
+texts = list(data["cleaned_description"].values)
 
-vectorizer = TfidfVectorizer()
-vectors = vectorizer.fit_transform(list(data["cleaned_description"].values) + [input_text]) 
+model = SentenceTransformer("paraphrase-MiniLM-L6-v2")
 
-input_vector = vectors[-1]
-similarities = cosine_similarity(input_vector, vectors[:-1]).flatten()
+text_embeddings = model.encode(texts, convert_to_tensor=True)
+input_embedding = model.encode(input_text, convert_to_tensor=True)
 
-sorted_indices = np.argsort(similarities)[::-1]
-sorted_texts = [data.iloc[i] for i in sorted_indices]
+similarities = util.pytorch_cos_sim(input_embedding, text_embeddings).flatten()
+
+sorted_indices = similarities.argsort(descending=True)
+sorted_texts = [data.iloc[i] for i in sorted_indices.cpu().data.numpy()]
 
 for i, row in enumerate(sorted_texts):
     if similarities[sorted_indices[i]] == 0 or i > 5:
         break
-    print(f"{i+1}. {row["code"]} ({row["all_names"]}) (Similarity: {similarities[sorted_indices[i]]:.4f})")
+    print(
+        f"{i+1}. {row["code"]} ({row["all_names"]}) (Similarity: {similarities[sorted_indices[i]]:.4f})"
+    )
