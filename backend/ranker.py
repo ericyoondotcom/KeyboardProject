3d3e715c195acb0cb982b8220c14976622cd61bc
Joshua Chen <sidachen2003@gmail.com>
2024-10-12 12:07:03 -0400

    Real-time ranker

--- backend/ranker.py
+++ backend/ranker.py
@@ -1,24 +1,23 @@
 from sentence_transformers import SentenceTransformer, util
 import pandas as pd
+import sys
 
 data = pd.read_csv("emoji_transformed.csv", keep_default_na=False)
 
-input_text = "vegetable"
 texts = list(data["cleaned_description"].values)
 
 model = SentenceTransformer("paraphrase-MiniLM-L6-v2")
 
 text_embeddings = model.encode(texts, convert_to_tensor=True)
-input_embedding = model.encode(input_text, convert_to_tensor=True)
 
-similarities = util.pytorch_cos_sim(input_embedding, text_embeddings).flatten()
+print("Model ready")
 
-sorted_indices = similarities.argsort(descending=True)
-sorted_texts = [data.iloc[i] for i in sorted_indices.cpu().data.numpy()]
-
-for i, row in enumerate(sorted_texts):
-    if similarities[sorted_indices[i]] == 0 or i > 5:
-        break
-    print(
-        f"{i+1}. {row["code"]} ({row["all_names"]}) (Similarity: {similarities[sorted_indices[i]]:.4f})"
-    )
+for input_text in sys.stdin:
+    input_embedding = model.encode(input_text, convert_to_tensor=True)
+    similarities = util.pytorch_cos_sim(input_embedding, text_embeddings).flatten()
+    sorted_indices = similarities.argsort(descending=True)[:5]
+    sorted_texts = [data.iloc[i] for i in sorted_indices.cpu().data.numpy()]
+    for i, row in enumerate(sorted_texts):
+        print(
+            f"{i+1}. {row["code"]} ({row["all_names"]}) (Similarity: {similarities[sorted_indices[i]]:.4f})"
+        )
